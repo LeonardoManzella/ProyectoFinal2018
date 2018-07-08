9db6c32cb7e5ed3c3d1d396247b59640a13187ba
Nicole Tobal <nickytobal@gmail.com>
2018-07-08 13:01:41 -0300

    Chatbot menu added

--- app/imports/ui/components/MainLayout.js
+++ app/imports/ui/components/MainLayout.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MainHeaderContainer from './sharedComponents/MainHeaderContainer';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { push as Menu } from 'react-burger-menu';
 
-// App component - represents the whole app
 export default class MainLayout extends Component {
-  render() {
-    const { content } = this.props;
 
+  adjustScreen(event) {
+    if (event.isOpen) {
+      document.getElementById("page-wrap").setAttribute("class", "page-wrapper");
+    } else {
+      document.getElementById("page-wrap").removeAttribute("class", "page-wrapper");
+    }
+  }
+
+  getBurgerIcon() {
     return (
       <div>
-        <MainHeaderContainer
-          currentUrl = {FlowRouter.current().path}
-        />
-        {content}
+        <img src="/img/chatbot.png" />
+      </div>
+    );
+  }
+
+  render() {
+    const { content } = this.props;
+    return (
+      <div>
+        <Menu
+          onStateChange={this.adjustScreen.bind(this)}
+          customBurgerIcon={this.getBurgerIcon()}
+          right noOverlay
+        >
+          <a>Chatbot</a>
+        </Menu>
+        <main id="page-wrap">
+          <MainHeaderContainer
+            currentUrl = {FlowRouter.current().path}
+          />
+          {content}
+        </main>
       </div>
     );
   }
