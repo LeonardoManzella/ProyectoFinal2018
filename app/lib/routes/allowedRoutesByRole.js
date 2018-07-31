57ea961e4fee2439215bad39ed4ebb6708143459
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-07-31 09:37:11 -0300

    Maintenance and Demo Admin

--- app/lib/routes/allowedRoutesByRole.js
+++ app/lib/routes/allowedRoutesByRole.js
@@ -2,7 +2,9 @@ const allowedRoutesByRole = {
   entrepreneur: ['binnacle', 'tasksBoard', 'expert', 'chart', 'chatbot', 'canvas', 'planList',
     'swot', 'risks'],
   administrator: ['binnacle', 'tasksBoard', 'expert', 'usersList', 'chart', 'chatbot', 'reminders',
-    'canvas', 'planList', 'swot', 'risks']
+    'canvas', 'planList', 'swot', 'risks'],
+  adminAdmin: ['binnacle', 'tasksBoard', 'expert', 'usersList', 'chart', 'chatbot', 'reminders',
+    'canvas', 'planList', 'swot', 'risks'] //For demo and emergency maintanance
 };
 
 export default allowedRoutesByRole;
