016d55d7edf84d3e2bf0b60a52541fc9971148fd
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-08-19 19:08:35 -0300

    Change Deploy Port

--- app/.deploy/mup.js
+++ app/.deploy/mup.js
@@ -26,9 +26,10 @@ module.exports = {
     env: {
       // TODO: Change to your app's url
       // If you are using ssl, it needs to start with https://
-      ROOT_URL: 'http://178.128.229.73:3000',
+      ROOT_URL: 'http://178.128.229.73',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
+      PORT: 3000,
     },
 
     docker: {
@@ -36,6 +37,7 @@ module.exports = {
       image: 'abernix/meteord:node-8.4.0-base',
     },
 
+    deployCheckPort: 3000,
     // Show progress bar while uploading bundle to server
     // You might need to disable it on CI servers
     enableUploadProgressBar: true
