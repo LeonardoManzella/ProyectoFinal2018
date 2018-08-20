4662b4cf7b5dad2d12eee419792745d1690a7059
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-08-19 23:08:57 -0300

    Adding Database Path

--- bin/start-mongodb.sh
+++ bin/start-mongodb.sh
@@ -5,6 +5,7 @@
 
 LOG_PATH="/data/logs/"
 LOG_FILE="mongod-botigo.log"
+DB_PATH="/data/db-botigo/"
 PORT="27059"
 
 echo ""
@@ -13,7 +14,7 @@ echo "### Starting MongoDB"
 echo "### LOG_PATH: $LOG_PATH"
 echo "### LOG_FILE: $LOG_FILE"
 echo "### PORT: $PORT"
-echo "### EXECUTING: sudo mongod --port $PORT --fork --logpath $LOG_PATH$LOG_FILE"
+echo "### EXECUTING: sudo mongod --port $PORT --fork --logpath $LOG_PATH$LOG_FILE --dbpath $DB_PATH"
 echo ""
 echo ""
-sudo mongod --port $PORT --fork --logpath $LOG_PATH$LOG_FILE && echo "### MongoDB Started" && echo "########################################"
+sudo mongod --port $PORT --fork --logpath $LOG_PATH$LOG_FILE --dbpath $DB_PATH && echo "### MongoDB Started" && echo "########################################"
