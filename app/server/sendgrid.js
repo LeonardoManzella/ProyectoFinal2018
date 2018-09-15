f2390e289be054d4df62cbb94bcdc4613d7d4c27
Leonardo Manzella <leonardo.manzella@gmail.com>
2018-09-15 19:54:30 -0300

    Fix sendgrid component

--- app/server/sendgrid.js
+++ app/server/sendgrid.js
@@ -6,28 +6,17 @@
 // echo "sendgrid.env" >> .gitignore
 // source ./sendgrid.env
 
-// Leyendo https://forums.meteor.com/t/sendgrid-examples/19965/4, me parece que hay que rehacerlo para usar una meteor call
-// sgMail.setApiKey('TODO API KEY');
-// const msg = {
-//   to: 'leonardo.manzella@gmail.com',
-//   from: 'leonardo.manzella@gmail.com',
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
 import { Meteor } from 'meteor/meteor';
 
 const FROM_EMAIL = 'leonardo.manzella@gmail.com'; //TODO move to settings json
+const API_KEY = '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' //TODO move to settings json
 
 if (Meteor.isServer) {
-	//var sendgrid = Meteor.npmRequire( '@sendgrid/client' )( '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');
 	import sendgrid from '@sendgrid/mail';
-	console.log(sendgrid);
-	sendgrid.setApiKey('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');
+	sendgrid.setApiKey(API_KEY);
 	
     Meteor.methods({
-      'sendgrid.sendEmail'( templateId, emailAddr, subject, bodyVar, otherVars ) {
+      'sendgrid.sendEmail'( templateId, emailAddr, subject, scheduled_tasks ) {
 		console.log("Callend Sendgrid to send email");
 		const msg = {
 			to: emailAddr,
@@ -41,7 +30,7 @@ if (Meteor.isServer) {
 				subject: 'Testing Templates',
 				name: 'Some One',
 				city: 'Denver',
-				plans:{
+				plans: scheduled_tasks /*{
 					communication_plan:[
 						{
 							"tool":"description",
@@ -78,46 +67,11 @@ if (Meteor.isServer) {
 							"supervisor":"supervisor2" 
 						}
 					]
-				},
+				}*/,
 			},
 		};
 		sendgrid.send(msg);
 		console.log( '========== MAIL SENT ==========' );
-		
-		/*
-		let email = new sendgrid.Email( {
-			to: emailAddr,
-			from: 'leonardo.manzella@gmail.com',  //TODO change this
-			subject: subject,
-			html: bodyVar, // <%body%> tag for text, required 
-			text: '' // <%body%> tag for html
-		} );
-
-		email.setFromName('leonardo.manzella@gmail.com');  // Optional
-		console.log("Name Set");
-		// Merge variables in template if any 
-		_.each( otherVars, function ( el ) {
-			for( key in el ) {
-				if( el.hasOwnProperty( key ) ) {
-					var value = el[ key ];
-					email.addSubstitution( key, value );
-				}
-			}
-		} );
-
-		console.log("Merged Variables");
-		// https://sendgrid.com/docs/API_Reference/SMTP_API/apps.html#template
-		email.addFilter( 'templates', 'template_id', templateId );
-
-		cconsole.log("Template selected");
-		sendgrid.send( email, function ( err, response ) {
-			if( err ) {
-				return console.error( err );
-			}
-			console.log( response ); // Success message
-		console.log( '========== MAIL SENT ==========' );
-	} );
-	*/
 	}
 	});
 }
