e8a3214935d6faab7503a74954a44647fe1cfb19
Martin Vergara <martin@Martins-MacBook-Pro.local>
2018-10-21 21:48:12 -0300

    Improve review interview styles

--- app/imports/ui/components/expertChatbot/ReviewInterview.jsx
+++ app/imports/ui/components/expertChatbot/ReviewInterview.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import ChatBot, { ChatBotUtil } from './../../../../lib/interview-chatbot/lib/index'
-import Logic from './Logic.js'
 import { TAPi18n } from 'meteor/tap:i18n';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import InterviewQuestions from './InterviewQuestions';
@@ -20,24 +18,63 @@ export class ReviewInterview extends React.Component {
 
 		return (
 			<div className="content-body plan">
-        <div className="col-md-6">
-          <h2>Respuestas de la Entrevista</h2>
-          {
-            InterviewQuestions.allQuestions.map((question, index) => (
-              <div key={index}>
-                <li>{TAPi18n.__('interview.' + question.question)}</li>
-                {
-                  question.possibleAnswers
-                    .filter(possibleAnswer => answers.includes(possibleAnswer))
-                    .map((possibleAnswer, answerIndex) =>
-                      <h6 key={answerIndex}>{TAPi18n.__('interview.' + possibleAnswer)}</h6>
-                    )
-                }
-              </div>
-            ))
+        <div className="col-md-12">
+            <div className="row" style={{alignItems: 'center', marginBottom: '20px'}}>
+                <strong><h1 style={{display: 'inline'}}>Revisión de Entrevista</h1></strong>
+                <img style={{height: '50px', marginLeft: '20px'}} src='/img/check.png' />
+            </div>
+            {
+            InterviewQuestions.allQuestions.map((question, index) => {
+              if(question.question == 'perpetual_identity') return '';
+              
+              if(question.question == 'what_words_1') {
+                return (<div key={index}>
+                    <button className='reviewInterviewQuestion'>¿Con qué palabras te identificás?</button>
+                    <div className='reviewInterviewAnswers'>
+                    <i><h4>Respondiste: </h4></i>
+                      {
+                        question.possibleAnswers
+                          .filter(possibleAnswer => answers.includes(possibleAnswer))
+                          .map((possibleAnswer, answerIndex) =>
+                            <button className='reviewInterviewAnswer' key={answerIndex}>{TAPi18n.__('interview.' + possibleAnswer)}</button>
+                          )
+                      }
+                    </div>
+                </div>)
+              }
+
+              if(question.question == 'what_words_2') {
+                return (<div key={index}>
+                        <div className='reviewInterviewAnswers'>
+                        {
+                            question.possibleAnswers
+                              .filter(possibleAnswer => answers.includes(possibleAnswer))
+                              .map((possibleAnswer, answerIndex) =>
+                                <button className='reviewInterviewAnswer'key={answerIndex}>{TAPi18n.__('interview.' + possibleAnswer)}</button>
+                              )
+                          }
+                        </div>
+                </div>)
+              }
+              
+              
+              return (<div key={index}>
+                  <button className='reviewInterviewQuestion'>{TAPi18n.__('interview.' + question.question)}</button>
+                      <div className='reviewInterviewAnswers'>
+                          <i><h4>Respondiste: </h4></i>
+                          {
+                          question.possibleAnswers
+                            .filter(possibleAnswer => answers.includes(possibleAnswer))
+                            .map((possibleAnswer, answerIndex) =>
+                              <button className='reviewInterviewAnswer'key={answerIndex}>{TAPi18n.__('interview.' + possibleAnswer)}</button>
+                            )
+                         }
+                    </div>
+              </div>)
+            })
           }
         </div>
-        <div className="row">
+        <div className="row" style={{marginTop: '20px'}}>
           <button className='pink-button' onClick={() => FlowRouter.go('chatbot')}>Volver</button>
         </div>
 			</div>
